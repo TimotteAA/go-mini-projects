41814f927aa1eace49748cad6a02871410420414
ngur.he <ngur.he@ucloud.cn>
2023-11-10 18:21:35 +0800

    定义cli命令

--- project1-local-books-cli/main.go
+++ project1-local-books-cli/main.go
@@ -1,6 +1,30 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
 	bookEntity := NewBookEntity()
-	bookEntity.Create()
+	
+	// 定义命令行的参数
+	flag.Parse();
+	// 获取非标志参数（子命令）
+    args := flag.Args()
+    if len(args) < 1 {
+        fmt.Println("No command provided")
+        os.Exit(1)
+    }
+
+    // 根据子命令调用不同的函数
+    switch args[0] {
+    case "create":
+        bookEntity.Create()
+    // 可以添加更多的子命令
+    default:
+        fmt.Printf("Unknown command: %s\n", args[0])
+        os.Exit(1)
+    }
 }
